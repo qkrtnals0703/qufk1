e2395ccc52924b256ed45fc6bd3bb2de29d2417f
qkrtnals0703 <qkrtnals7428@naver.com>
2021-09-05 22:56:36 +0900

    자바에센셜 2장 보너스 문제

---
+++ src/Arithmetic.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("식을 입력하세요>>");
+        double op1 = scanner.nextDouble();
+        String operater = scanner.next();
+        double op2 = scanner.nextDouble();
+        double result = 0;
+        switch (operater){
+            case "+" : result = op1 + op2; break;
+            case "-" : result = op1 - op2; break;
+            case "*" : result = op1 * op2; break;
+            case "/" :
+                if(op2 == 0){
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return;
+                }
+                result = op1 / op2;
+                break;
+            default: System.out.println("연산 기호가 잘못되었습니다.");
+        }
+        System.out.println("연산 결과 " + result);
+        scanner.close();
+    }
+}

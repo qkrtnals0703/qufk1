332d99fa3730f58d72d944b0e12f17d138c07e94
qkrtnals0703 <qkrtnals7428@naver.com>
2021-09-22 19:11:35 +0900

    String, 백준 1712

---
+++ src/Bj_1712.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bj_1712 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+        if(C <= B){
+            System.out.println("-1");
+        }
+        else {
+            System.out.println((A/(C-B))+1);
+        }
+    }
+}
